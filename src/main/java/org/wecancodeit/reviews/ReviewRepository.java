c5b3e3e81bf037f05c6cca64e49fb708a48b572b
Izzy Elwyn <izzy.elwyn@gmail.com>
2018-10-02 08:39:31 -0400

    style and what not

--- src/main/java/org/wecancodeit/reviews/ReviewRepository.java
+++ src/main/java/org/wecancodeit/reviews/ReviewRepository.java
@@ -12,15 +12,19 @@ public class ReviewRepository {
 	private Map<Long, Review> reviewList = new HashMap<>();
 	
 	public ReviewRepository() {
-		Review thresher = new Review(1, "Thresher Shark", "images/ThresherShark.jpg", "Sharks", "Extra Long Caudal Fin used to Whip the water and stun fish", "5 out of 5 Chomps");
-		Review frilled = new Review(2, "Frilled Shark", "images/FrilledShark.jpg", "Sharks", "Deep Sea Sharks, like something from a prehistoric age", "5 out of 5 Chomps");
-		Review whale = new Review(3, "Whale Shark", "images/WhaleShark.jpg", "Shark", "Gentle Giants, the largest fish", "5 out of 5 filter feeders");
-		Review bull = new Review (4, "Bull Shark", "images/BullShark.jpg", "Shark", "Can switch from salt water to fresh water, most likely to bite humans", "5 out of 5 terrifying Chomps");
+		Review thresher = new Review(1, "Thresher Shark", "images/ThresherShark.jpg", "Alopiidae", "They use their elongated caudal fin to whip the water around fish and stun them. It doesn't get cooler than this.", "5 out of 5 Chomps");
+		Review greatWhite = new Review(2, "Great White Shark", "images/GreatWhite.jpg","Lamnidae","Classic. Elegant.", "5 out of 5 chomps");
+		Review frilled = new Review(3, "Frilled Shark", "images/FrilledShark.jpg", "Chlamydoselachidae", "They look like freaking sea serpents! A+", "5 out of 5 chomps");
+		Review whale = new Review(4, "Whale Shark", "images/WhaleShark.jpg", "Cetorhinidae", "Gentle Giants, the largest fish", "5 out of 5 filter feeders");
+		Review sandTiger = new Review(5, "Sand Tiger Shark", "images/SandTiger.jpg","Odontaspididae","Will make you happy your parents made you get braces", "5 out of 5 chomps");
+		Review bull = new Review (6, "Bull Shark", "images/BullShark.jpg", "Carcharhinidae", "Can switch from salt water to fresh water, most likely to bite humans", "5 out of 5 terrifying chomps");
 		
 		reviewList.put(thresher.getId(), thresher);
 		reviewList.put(frilled.getId(), frilled);
 		reviewList.put(whale.getId(), whale);
 		reviewList.put(bull.getId(), bull);
+		reviewList.put(greatWhite.getId(), greatWhite);
+		reviewList.put(sandTiger.getId(), sandTiger);
 			
 	}
 	
